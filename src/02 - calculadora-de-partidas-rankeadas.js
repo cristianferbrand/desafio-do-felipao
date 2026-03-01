1dd0dd93c8d4afec9c1693af32704e182b3c5f8f
cristianferbrand <cristianferbrand@gmail.com>
2026-03-01 13:54:33 -0300

    Commit 2° Desafio

--- src/02 - calculadora-de-partidas-rankeadas.js
+++ src/02 - calculadora-de-partidas-rankeadas.js
@@ -1,20 +1,30 @@
 function calcularSaldo(vitorias, derrotas) {
-  return vitorias - derrotas;
+    return vitorias - derrotas;
 }
 
 function calcularNivel(vitorias) {
-  let nivel = "";
+    let nivel = "";
 
-  if (vitorias <= 10) nivel = "Ferro";
-  else if (vitorias <= 20) nivel = "Bronze";
-  else if (vitorias <= 50) nivel = "Prata";
-  else if (vitorias <= 80) nivel = "Ouro";
-  else if (vitorias <= 90) nivel = "Diamante";
-  else if (vitorias <= 100) nivel = "Lendário";
-  else nivel = "Imortal";
-
-  return nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Ferro";
+    }
+    return nivel;
 }
+
 
 // Exemplo de uso
 let vitorias = 15;
