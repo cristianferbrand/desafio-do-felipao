990d1e9ff7f467d0368234188094184bcb82aeb6
cristianferbrand <cristianferbrand@gmail.com>
2026-03-02 23:11:30 -0300

    Commit 3° desafio

--- src/03 - escrevendos-as-classes-de-um-jogo.js
+++ src/03 - escrevendos-as-classes-de-um-jogo.js
@@ -1,24 +1,23 @@
-class hero {
+class Hero {
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
-    atacar(){
-        if(this.tipo === "mago"){
+    atacar() {
+        if (this.tipo === "mago") {
             console.log(`o ${this.tipo} atacou usando magia`);
-        }
-        if(this.tipo === "guerreiro"){
+        } else if (this.tipo === "guerreiro") {
             console.log(`o ${this.tipo} atacou usando espada`);
-        }
-        if(this.tipo === "monge"){
+        } else if (this.tipo === "monge") {
             console.log(`o ${this.tipo} atacou usando artes marciais`);
-        }
-        if(this.tipo === "ninja"){
+        } else if (this.tipo === "ninja") {
             console.log(`o ${this.tipo} atacou usando shuriken`);
+        }else {
+            console.log(`o ${this.tipo} atacou usando um ataque básico`);
         }
     }
 }
 
-let heroMagico = new hero("gandalf", 1000, "mago");
+let heroMagico = new Hero("gandalf", 1000, "mago");
 heroMagico.atacar();
